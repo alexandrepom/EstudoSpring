71ebe76d8a5da4d4e39f93c5dc88ac8e41a5c45b
Alexandre Gonzaga <alexandregonzaga@gmail.com>
2018-11-23 16:49:15 -0200

    Implementado o domínio Categoria. Ajuste realizado na classe ResourceCategoria para retornar a categoria definida

--- src/main/java/br/com/gonzaga/resource/CategoriaResource.java
+++ src/main/java/br/com/gonzaga/resource/CategoriaResource.java
@@ -1,15 +1,42 @@
+/**
+ * Classe utilizada para controlar as chamadas REST ao endpoint "/categorias".
+ * Ela está vinculada ao domínio Categorias.
+ *   
+ * @author Alexandre Gonzaga
+ * @version 1.0
+ * 
+ * 
+ * */
+
 package br.com.gonzaga.resource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.gonzaga.domain.Categoria;
+
 @RestController //controller especializado para resposta REST
 @RequestMapping(value="/categorias") //endpoint "categorias" da API REST
 public class CategoriaResource {
 	
+	/**
+	 * Responde as requests GET HTTP devolvendo as categorias
+	 * @return a lista de categorias 
+	 * */
 	@RequestMapping(method=RequestMethod.GET) //atribui o método GET do HTTP à função listar()
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
