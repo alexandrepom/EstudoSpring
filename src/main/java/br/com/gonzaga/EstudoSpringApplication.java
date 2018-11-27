2e7f807b773f7042131f584cdf6ff8a48d1bd962
Alexandre Gonzaga <alexandregonzaga@gmail.com>
2018-11-26 22:23:43 -0200

    criado a inicialização das instancias da categoria

--- src/main/java/br/com/gonzaga/EstudoSpringApplication.java
+++ src/main/java/br/com/gonzaga/EstudoSpringApplication.java
@@ -1,12 +1,35 @@
 package br.com.gonzaga;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudoSpringApplication {
+import br.com.gonzaga.domain.Categoria;
+import br.com.gonzaga.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class EstudoSpringApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoSpringApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		repo.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
+	
+	
+	
 }
