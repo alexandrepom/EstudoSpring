8b38597b8c09b78797b53a79bc0fa11a8cf92d7f
Alexandre Gonzaga <alexandregonzaga@gmail.com>
2018-12-03 22:27:19 -0200

    Correção em CategoriasServices e Pagamento

--- src/main/java/br/com/gonzaga/services/CategoriaService.java
+++ src/main/java/br/com/gonzaga/services/CategoriaService.java
@@ -42,7 +42,8 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
+		Categoria newObj = find(obj.getId());
+		obj.setNome(newObj.getNome());;
 		return repo.save(obj);
 	}
 
